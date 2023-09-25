575039ecceb07f889acaf4a85e3cc2abb0d1ebc9
Fayti1703 <fayti1703@protonmail.com>
2023-09-25 21:40:21 +0200

    Drop old LocaleData.ReplaceParameters method
    Superseded by QRules' StringStatement

--- Ktisis/Localization/LocaleData.cs
+++ Ktisis/Localization/LocaleData.cs
@@ -31,39 +31,4 @@ public class LocaleData {
 	public bool HasTranslationFor(string key) {
 		return this._translationData.ContainsKey(key);
 	}
-
-	private string ReplaceParameters(string handle, string translationString, Dictionary<string, string>? parameters) {
-		StringBuilder result = new(translationString.Length);
-		StringBuilder key = new(16);
-		bool inParameter = false;
-		foreach(char c in translationString) {
-			if(!inParameter) {
-				if(c == '%')
-					inParameter = true;
-				else
-					result.Append(c);
-			} else {
-				if(c == '%') {
-					if(key.Length == 0) /* '%%' escape sequence */
-						result.Append('%');
-					else {
-						string keyV = key.ToString();
-						string? value = null;
-						parameters?.TryGetValue(keyV, out value);
-						if(value == null) {
-							Logger.Warning("Unassigned parameter '{0}' in value for '{1}' in locale '{2}'", keyV, handle, this.MetaData.TechnicalName);
-							value = $"%{keyV}%";
-						}
-						result.Append(value);
-						key.Clear();
-					}
-					
-					inParameter = false;
-				} else {
-					key.Append(c);
-				}
-			}
-		}
-		return result.ToString();
-	}
 }
