14e991205f02a32631f792156f8c00d43b8eebfc
Fayti1703 <fayti1703@protonmail.com>
2023-09-25 21:42:42 +0200

    BBJR: `State` -> `Stage`

--- Ktisis/Data/Json/BlockBufferJsonReader.cs
+++ Ktisis/Data/Json/BlockBufferJsonReader.cs
@@ -10,7 +10,7 @@ namespace Ktisis.Data.Json;
  */
 public ref struct BlockBufferJsonReader {
 
-	private enum State {
+	private enum Stage {
 		INIT,
 		READING,
 		FINAL_READ,
@@ -22,7 +22,7 @@ public ref struct BlockBufferJsonReader {
 	private readonly Span<byte> blockBuffer;
 	private Span<byte> readSlice = default;
 	private JsonReaderState jsonState;
-	private State state = State.INIT;
+	private Stage _stage = Stage.INIT;
 
 	public BlockBufferJsonReader(Stream stream, Span<byte> blockBuffer, JsonReaderOptions options) {
 		this.stream = stream;
@@ -31,20 +31,20 @@ public ref struct BlockBufferJsonReader {
 	}
 
 	public bool Read() {
-		switch(this.state) {
-			case State.CLOSED:
+		switch(this._stage) {
+			case Stage.CLOSED:
 				return false;
-			case State.READING:
-			case State.FINAL_READ:
+			case Stage.READING:
+			case Stage.FINAL_READ:
 				if(this.Reader.Read()) return true;
-				if(this.state == State.FINAL_READ) {
-					this.state = State.CLOSED;
+				if(this._stage == Stage.FINAL_READ) {
+					this._stage = Stage.CLOSED;
 					return false;
 				}
-				goto case State.INIT;
-			case State.INIT:
+				goto case Stage.INIT;
+			case Stage.INIT:
 				this.acquireReader();
-				goto case State.READING;
+				goto case Stage.READING;
 		}
 		Debug.Assert(false, "This point is unreachable");
 		throw new Exception("This point is unreachable");
@@ -52,8 +52,8 @@ public ref struct BlockBufferJsonReader {
 
 	private void acquireReader() {
 		int preRead = 0;
-		Debug.Assert(this.state != State.FINAL_READ, "Shouldn't be in final read here");
-		if(this.state != State.INIT) {
+		Debug.Assert(this._stage != Stage.FINAL_READ, "Shouldn't be in final read here");
+		if(this._stage != Stage.INIT) {
 			if(this.Reader.BytesConsumed == 0)
 				throw new Exception("JSON value appears to exceed the bounds of the block buffer. Increase the buffer size or decrease your JSON value size.");
 			this.jsonState = this.Reader.CurrentState;
@@ -64,7 +64,7 @@ public ref struct BlockBufferJsonReader {
 
 		int read = this.stream.Read(this.blockBuffer[preRead..]);
 		this.readSlice = this.blockBuffer[..(preRead+read)];
-		this.state = this.readSlice.Length == 0 ? State.FINAL_READ : State.READING;
+		this._stage = this.readSlice.Length == 0 ? Stage.FINAL_READ : Stage.READING;
 
 		this.Reader = new Utf8JsonReader(this.readSlice, this.readSlice.Length == 0, this.jsonState);
 	}
